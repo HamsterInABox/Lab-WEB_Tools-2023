42131c591f5e5aea04bd6a3e09062f86e849ac20
HamsterInABox <134917933+HamsterInABox@users.noreply.github.com>
2023-10-27 13:55:45 +0300

    Update index.php

--- Asgmt_10_PHP/index.php
+++ Asgmt_10_PHP/index.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Hi </title>
 
+<style>
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: DarkBlue;
+  color: white;
+}
+</style>
+</head>
+
+<body>
+
+<footer>
+<?php
+	date_default_timezone_set("Europe/Riga");
+	echo date("Y-m-d h:i:sa");
+	echo "   ";
+   ?>
+</footer>
+</body>
+</html>
